d2e90c07ea5d8844bac401b1b7762e3ef652ab7e
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-09-13 10:52:25 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;  // Added "!" at the end
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
